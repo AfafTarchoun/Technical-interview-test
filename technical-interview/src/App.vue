4cbb86f19447bcda330624c2f2052e9c52fef74b
Afaf Tarchoun <tarchounafaf@gmail.com>
2023-12-15 15:33:02 +0100

    Adding Search Form component in the app.vue

---
+++ technical-interview/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <!-- The main application container -->
+  <div id="app">
+    <SearchForm />
+    <div class="gif-display"></div>
+  </div>
+</template>
+
+<script>
+// Import the SearchForm component
+import SearchForm from './components/SearchForm.vue';
+
+export default {
+  components: {
+    SearchForm
+  }
+};
+</script>
