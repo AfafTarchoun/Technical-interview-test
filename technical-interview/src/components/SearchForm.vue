b84b3ce629a703cd8f74df9288df443497995d39
Afaf Tarchoun <tarchounafaf@gmail.com>
2023-12-15 15:53:26 +0100

    Using searchGIFs to fetch data using API url while replacing API key in the url, converting the response to JSON, TO DO: make the display function

--- technical-interview/src/components/SearchForm.vue
+++ technical-interview/src/components/SearchForm.vue
@@ -15,6 +15,19 @@
     },
     methods: {
       async searchGIFs() {
+            // Prevent the form's default behavior of submitting and reloading the page
+                if (this.searchTerm.trim() !== '') { // Check if the search term is not empty
+                try {
+                    // Fetch GIFs based on the search term from API
+                    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=UC6QeKH1sTZwo7OgHc1oAJJu4JFV59TJ&q=${this.searchTerm}&limit=25&offset=0&rating=g&lang=en`);
+                    // Convert the response to JSON format
+                    const data = await response.json();
+                    //TO DO : display GIF function using the data
+                } catch (error) {
+                    // Catching any fetching error
+                    console.error('Error fetching data:', error);
+                }
+                }
             },
         }
     }
