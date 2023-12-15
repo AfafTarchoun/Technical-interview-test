6a439b35480b5b975b5d0560f9c3ad75be7ccdf3
Afaf Tarchoun <tarchounafaf@gmail.com>
2023-12-15 16:10:52 +0100

    Displaying the GIFs retrieved from the Giphy API on the webpage using displayGIFs method

--- technical-interview/src/components/SearchForm.vue
+++ technical-interview/src/components/SearchForm.vue
@@ -14,21 +14,35 @@
       };
     },
     methods: {
+        //async function to search for GIF using the given API
       async searchGIFs() {
             // Prevent the form's default behavior of submitting and reloading the page
                 if (this.searchTerm.trim() !== '') { // Check if the search term is not empty
-                try {
-                    // Fetch GIFs based on the search term from API
-                    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=UC6QeKH1sTZwo7OgHc1oAJJu4JFV59TJ&q=${this.searchTerm}&limit=25&offset=0&rating=g&lang=en`);
-                    // Convert the response to JSON format
-                    const data = await response.json();
-                    //TO DO : display GIF function using the data
-                } catch (error) {
-                    // Catching any fetching error
-                    console.error('Error fetching data:', error);
-                }
+                    try {
+                        // Fetch GIFs based on the search term from API
+                        const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=UC6QeKH1sTZwo7OgHc1oAJJu4JFV59TJ&q=${this.searchTerm}&limit=25&offset=0&rating=g&lang=en`);
+                        // Convert the response to JSON format
+                        const data = await response.json();
+                        this.displayGIFs(data.data);
+                    } catch (error) {
+                        // Catching any fetching error
+                        console.error('Error fetching data:', error);
+                    }
                 }
             },
+            //Display the GIFs on the webpage
+            displayGIFs(gifs) {
+                // Logic to display GIFs on the webpage
+                const gifContainer = document.querySelector('.gif-display');
+                gifContainer.innerHTML = ''; // Clear previous results
+
+                gifs.forEach(gif => {
+                    const img = document.createElement('img'); //Assuming a simple display of images
+                    img.src = gif.images.original.url;
+                    img.alt = gif.title;
+                    gifContainer.appendChild(img);
+                });
+            }
         }
     }
   </script>
