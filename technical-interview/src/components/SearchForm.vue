b5c6eb3726429f43e08c60ab67759924dd454af8
Afaf Tarchoun <tarchounafaf@gmail.com>
2023-12-15 15:44:26 +0100

    Creating Html form with the text input field and the submit button that prevents the default form submission and triggers searchGIFs function

---
+++ technical-interview/src/components/SearchForm.vue
@@ -0,0 +1,21 @@
+<template>
+    <!-- Create a form to input the search term -->
+    <form @submit.prevent="searchGIFs">
+      <input type="text" v-model="searchTerm" placeholder="Enter your search term">
+      <button type="submit">Search</button>
+    </form>
+  </template>
+
+  <script>
+  export default {
+    data() {
+      return {
+        searchTerm: '' // Holds the user's search term
+      };
+    },
+    methods: {
+      async searchGIFs() {
+            },
+        }
+    }
+  </script>
